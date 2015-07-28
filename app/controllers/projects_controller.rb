2f0a3d0dd5cbd7ce2fff173dd8ff07b2b2f9d225
Tubal Lara <tublargar@gmail.com>
2015-07-28 10:28:05 +0200

    Rescuing errors

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,11 @@
 class ProjectsController < ApplicationController
 	def index
-		@projects = Project.order("created_at DESC").limit(10)
-		
+		@projects = Project.order("created_at DESC").limit(10)		
+	end
+
+	def show
+		unless @project = Project.find_by(id: params[:id])
+			render 'no_projects_found'
+		end
 	end
 end
