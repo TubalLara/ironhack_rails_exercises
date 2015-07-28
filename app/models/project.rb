bf5ea6931ebf71fee662feee4845d246db5cb92c
Tubal Lara <tublargar@gmail.com>
2015-07-28 09:56:58 +0200

    moving logic to controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ActiveRecord::Base
 	def self.iron_find(id)
 		where("id=?", id).first
 	end
+
+	def self.clean_old
+		where("created_at < ?", Date.current - 7.days).delete_all		
+	end
+
+	def self.last_ten_created
+		order("created_at DESC").limit(10)
+	end
 end
